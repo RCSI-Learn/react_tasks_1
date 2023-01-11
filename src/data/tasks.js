5d5a2aed20e9694354596447f5a7bde9700d9b88
Roberto Carlos Siles Inca <rcsi.learn@gmail.com>
2023-01-10 21:17:41 -0400

    First commit.

---
+++ src/data/tasks.js
@@ -0,0 +1,27 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "first",
+    description: "first task",
+  },
+  {
+    id: 1,
+    title: "second",
+    description: "second task",
+  },
+  {
+    id: 2,
+    title: "third",
+    description: "third task",
+  },
+  {
+    id: 3,
+    title: "fourth ",
+    description: "fourth  task",
+  },
+  {
+    id: 4,
+    title: "five",
+    description: "five task",
+  },
+];
